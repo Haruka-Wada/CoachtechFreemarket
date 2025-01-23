dcef72d50000d89d1281187e0efa454ea6ed91b2
haruka wada <022.wadaha@gmail.com>
2025-01-23 13:29:51 +0900

    fortify追加

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+    public function address(Request $request)
+    {
+        $item_id = $request->item_id;
+        return view('auth.address', compact('item_id'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = User::find(Auth::id());
+        $user->update([
+            'post_code' => $request->post_code,
+            'address' => $request->address,
+            'building' => $request->building
+        ]);
+        $item_id = $request->item_id;
+        return redirect(route('item.purchase', [
+            'item_id' => $item_id
+        ]));
+    }
+
+    public function profile()
+    {
+        return view('auth.profile');
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::find(Auth::id());
+        $thumbnail = $request->file('thumbnail');
+        $path = $thumbnail->store('thumbnail', 'public');
+        $full_path = asset('storage/' . $path);
+        if (Auth::user()->thumbnail) {
+            $old_path = basename(Auth::user()->thumbnail);
+            Storage::disk('public')->delete('thumbnail/' . $old_path);
+        }
+
+        $user->update([
+            'name' => $request->name,
+            'post_code' => $request->post_code,
+            'address' => $request->address,
+            'building' => $request->building,
+            'thumbnail' => $full_path
+        ]);
+
+        return redirect('/mypage');
+    }
+}
---
+++ src/database/seeders/Category_itemTableSeeder.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use DateTime;
+
+class Category_itemTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('category_item')->insert([
+            [
+                'item_id' => 1,
+                'category_id' => 1,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 1,
+                'category_id' => 5,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 2,
+                'category_id' => 2,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 3,
+                'category_id' => 15,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 4,
+                'category_id' => 1,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 4,
+                'category_id' => 5,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 5,
+                'category_id' => 2,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 6,
+                'category_id' => 2,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 7,
+                'category_id' => 1,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 7,
+                'category_id' => 4,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 8,
+                'category_id' => 10,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 9,
+                'category_id' => 10,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+            [
+                'item_id' => 10,
+                'category_id' => 6,
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime()
+            ],
+        ]);
+    }
+}
